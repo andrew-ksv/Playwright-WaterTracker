cf8219ce75f21f25cb5fa39233426a3a6951e2c7
andrew-ksv <andrew-ksv@users.noreply.github.com>
2025-08-22 12:41:30 +0000

    deploy: 5de26baebcbb5ed9f8300dac52b3280756c45224

--- tests/utils/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from "@playwright/test";
-
-export async function authFrontend(page: Page, email: string, password: string) {
-  await page.fill('input[name="email"]', email);
-  await page.fill('input[name="password"]', password);
-  await page.click('button[type="submit"]');
-};
